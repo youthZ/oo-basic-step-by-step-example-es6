e863c8768c1fa2e2ba99e5ca856197cff94a70a3
youthZ <552099787@qq.com>
2017-12-04 04:58:19 +0000

    practice_2 is finished!

--- spec/practice_2/main_spec.js
+++ spec/practice_2/main_spec.js
@@ -1,37 +1,28 @@
 "use strict";
-import chai from "chai";
-import sinonChai from "sinon-chai";
-const expect = chai.expect;
-chai.use(sinonChai);
+//import chai from "chai";
+//import sinonChai from "sinon-chai";
+//const expect = chai.expect;
+//chai.use(sinonChai);
 
-import Person from "../../src/practice_2/person.js";
-import Student from "../../src/practice_2/student.js";
-
+const  Person = require ("../../src/practice_2/person");
+const  Student = require ("../../src/practice_2/student");
 describe("Person", () => {
     it("should have field name and age", () => {
         const person = new Person("Tom", 21);
-        expect(person.name).to.equal("Tom");
-        expect(person.age).to.equal(21);
+		console.log(person);
+        expect(person.name).toEqual("Tom");
+        expect(person.age).toEqual(21);
     });
 
     it("should have a method introduce, introduce person with name and age", () => {
         const person = new Person("Tom", 21);
         const introduce = person.introduce();
-        expect(introduce).to.equal("My name is Tom. I am 21 years old.");
+        expect(introduce).toEqual("My name is Tom. I am 21 years old.");
     });
 
-    describe("Student", () => {
-        it("should have field name, age and class number", () => {
-            const student = new Student("Tom", 21, 2);
-            expect(student.name).to.equal("Tom");
-            expect(student.age).to.equal(21);
-            expect(student.klass).to.equal(2);
-        });
-
-        it("should overwrite Person introduce, introduce student with class", () => {
+	 it("should overwrite Person introduce, introduce student with class", () => {
             const student = new Student("Tom", 21, 2);
             const introduce = student.introduce();
-            expect(introduce).to.equal("I am a Student. I am at Class 2.");
+            expect(introduce).toEqual("I am a Student. I am at Class 2.");
         });
-    })
 });
--- src/practice_2/student.js
+++ src/practice_2/student.js
@@ -1,1 +1,12 @@
-
+const  Person = require ("../../src/practice_1/person");
+class student extends Person
+{
+	constructor(name,age, klass){
+		super(name,age);
+		this.klass = klass; 
+	}
+	introduce(){
+		return "I am a Student. I am at Class "+this.klass+".";
+	}
+}
+module.exports = student;

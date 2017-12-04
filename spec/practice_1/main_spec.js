0eae5dba6d5616904487c0b314ce8b2ff76ca14b
youthZ <552099787@qq.com>
2017-12-04 04:38:07 +0000

    practice_1 is finished!

--- spec/practice_1/main_spec.js
+++ spec/practice_1/main_spec.js
@@ -1,21 +1,22 @@
+
 "use strict";
-import chai from "chai";
-import sinonChai from "sinon-chai";
-const expect = chai.expect;
-chai.use(sinonChai);
+//import chai from "chai";
+//import sinonChai from "sinon-chai";
+//const expect = chai.expect;
+//chai.use(sinonChai);
 
-import Person from "../../src/practice_1/person.js";
-
+const  Person = require ("../../src/practice_1/person");
 describe("Person", () => {
     it("should have field name and age", () => {
         const person = new Person("Tom", 21);
-        expect(person.name).to.equal("Tom");
-        expect(person.age).to.equal(21);
+		console.log(person);
+        expect(person.name).toEqual("Tom");
+        expect(person.age).toEqual(21);
     });
 
     it("should have a method introduce, introduce person with name and age", () => {
         const person = new Person("Tom", 21);
         const introduce = person.introduce();
-        expect(introduce).to.equal("My name is Tom. I am 21 years old.");
+        expect(introduce).toEqual("My name is Tom. I am 21 years old.");
     });
 });
